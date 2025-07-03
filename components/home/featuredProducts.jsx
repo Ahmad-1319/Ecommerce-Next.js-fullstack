b859b85bab0ea8b990df5b596ddb475b187b4a6e
Ahmad <ahmu5522@gmail.com>
2025-07-03 19:38:32 +0500

    fixed featured products issue

--- components/home/featuredProducts.jsx
+++ components/home/featuredProducts.jsx
@@ -9,9 +9,10 @@ const FeaturedProducts = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const res = await fetch("http://localhost:3000/api/products/get");
-        if (res.status === '200') {
+        const res = await fetch("/api/products/get");
+        if (res.ok) {
           const products = await res.json();
+          console.log("Fetched products:", products);
           setFeaturedProducts(products.filter((elem) => elem.featured));
         } else {
           setFeaturedProducts([]);
@@ -26,13 +27,16 @@ const FeaturedProducts = () => {
   }, []);
 
   if (loading) {
-    return <div className="text-center py-10">Loading featured products...</div>;
+    return <div className="text-center py-10 text-xl animate-pulse font-bold">Loading...</div>;
   }
 
   return (
     <>
          <h1 className='text-4xl sm:text-5xl text-center'>Featured Products</h1>
         <p className='my-5 text-slate-600 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam harum atque consequuntur?</p>
+        {featuredProducts.length === 0 ?(
+          <p className="text-center my-8 text-gray-800 font-semibold">No Products Found </p>
+        ):(
         <div className='grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-6  my-5 gap-3'>
   
           {featuredProducts.map((p , index)=>{
@@ -40,7 +44,8 @@ const FeaturedProducts = () => {
           })}
        
         </div>
-      
+        )
+}
     </>
   )
 }
